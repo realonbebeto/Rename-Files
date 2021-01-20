e47e9d458a3f994b02f2b44e44ff1fcf0a2474a8
Bebeto Nyamwamu <projobs254@gmail.com>
2021-01-20 11:17:06 +0300

    Renaming Multiple Files

--- Rename.py
+++ Rename.py
@@ -9,12 +9,12 @@ Created on Wed Jan 20 10:30:32 2021
 import os
 
 def main():
-    for count, filename in enumerate(os.listdir("/home/nyamwamu/Downloads/WQU Data Science")):
+    for count, filename in enumerate(os.listdir("folderpath")):
         old_name = str(filename)
         new_name = old_name.strip("yt1s.com -")
         fin_name = new_name.replace("_1080p", "")
-        src = "/home/nyamwamu/Downloads/WQU Data Science/" + filename
-        dst = "/home/nyamwamu/Downloads/WQU Data Science/" + fin_name
+        src = "folderpath" + filename
+        dst = "folderpath" + fin_name
         print(fin_name)
         os.rename(src, dst)
 
